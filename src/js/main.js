aa0ebf33aaf128d5b60c57a68b46354a25478cd4
Hakan Bilgin <hbi99@hotmail.com>
2021-05-10 23:54:52 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+
+const supermariobros = {
+	init() {
+		// fast references
+		this.content = window.find("content");
+	},
+	dispatch(event) {
+		switch (event.type) {
+			case "window.open":
+				break;
+			case "open-help":
+				defiant.shell("fs -u '~/help/index.md'");
+				break;
+		}
+	}
+};
+
+window.exports = supermariobros;
